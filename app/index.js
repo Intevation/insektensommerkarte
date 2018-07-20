0c88becac889afe4946d1379c1c7b169384c5a6a
Bjoern Schilberg <bjoern@intevation.de>
2018-07-20 12:25:40 +0200

    Added option to select the campain.

--- app/index.js
+++ app/index.js
@@ -17,6 +17,22 @@ import '../css/spinner.css';
 import '../css/index.css';
 import '../node_modules/taucharts/dist/plugins/tooltip.css';
 
+let searchParams = new URLSearchParams(window.location.search);
+let param;
+
+if (searchParams.has('campain')) {
+  param = searchParams.get('campain');
+  $('#campain').val(param);
+} else {
+  param = $('#campain option:selected').val();
+}
+
+console.log(param);
+
+$('#campain').on('change', '', function(e) {
+  window.location = '?campain=' + this.value;
+});
+
 // from uikit-icons.js
 var info = '<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"> <path d="M12.13,11.59 C11.97,12.84 10.35,14.12 9.1,14.16 C6.17,14.2 9.89,9.46 8.74,8.37 C9.3,8.16 10.62,7.83 10.62,8.81 C10.62,9.63 10.12,10.55 9.88,11.32 C8.66,15.16 12.13,11.15 12.14,11.18 C12.16,11.21 12.16,11.35 12.13,11.59 C12.08,11.95 12.16,11.35 12.13,11.59 L12.13,11.59 Z M11.56,5.67 C11.56,6.67 9.36,7.15 9.36,6.03 C9.36,5 11.56,4.54 11.56,5.67 L11.56,5.67 Z"></path> <circle fill="none" stroke="#000" stroke-width="1.1" cx="10" cy="10" r="9"></circle></svg>';
 
@@ -27,8 +43,6 @@ UIkit.icon.add({ info: info });
 if ($(window).width() < 599) {
   $('.intro-sidebar').hide();
   $('#sidebar header').hide();
-} else {
-  $('.intro-sidebar').html('Hier können Sie sich verschiedene Meldungen und Beobachtungen anzeigen lassen, um interaktiv die Daten des <a href="https://www.nabu.de/tiere-und-pflanzen/aktionen-und-projekte/insektensommer/index.html" target=_blank>Insektensommers</a> zu erkunden.');
 }
 
 var outerHeight = $('#details').outerHeight(!0);
@@ -378,11 +392,11 @@ function top5bundesland(features, bundesland) {
   return top5;
 }
 
-function anzahlLebensraum(features, lebensraum) {
-  let beobachtungen = features.filter(feature => feature.properties.lebensraum === lebensraum);
-
-  return { beobachtungen: beobachtungen.length, meldungen: anzahlMeldungen(beobachtungen) };
-}
+// function anzahlLebensraum(features, lebensraum) {
+//   let beobachtungen = features.filter(feature => feature.properties.lebensraum === lebensraum);
+//
+//   return { beobachtungen: beobachtungen.length, meldungen: anzahlMeldungen(beobachtungen) };
+// }
 
 function lebensraumTop5(features, lebensraum) {
   const l = features.filter(feature => feature.properties.lebensraum === lebensraum);
@@ -415,10 +429,18 @@ function bundeslaenderTOP5(features) {
 map.on('load', function() {
   $('#sidebar').css('left', '50px');
   $.ajax({
-    url: 'data/beobachtungen.csv',
+    url: 'data/beobachtungen-' + param + '.csv',
     method: 'GET',
     success: function(data) {
       makeGeoJSON(data);
+      $('#mySpinner').hide();
+    },
+    error: function() {
+      UIkit.sticky('.sticky', { offset: 50, top: 200 });
+
+      // Functional components should omit the 'element' argument.
+      UIkit.notification('Keine Daten für Aktionszeitraum ' + param + ' gefunden!', 'danger');
+      console.log('Keine Daten für Aktionszeitraum ' + param + ' gefunden!')
       $('#mySpinner').hide();
     }
   });
