b07765da7232d72af9b27e09ca4c1cd032d0f9a7
Danny Reichelt <Danny Reichelt dreichelt@intevation.de>
2024-04-03 14:39:50 +0200

    Anpassung Einleitungstext, Individuen, Meldungen

--- app/index.js
+++ app/index.js
@@ -372,7 +372,15 @@ function anzahlMeldungen(features) {
     })
     .reverse()
     .map(JSON.parse);
-  return meldungen.length;
+
+  return meldungen.length.toLocaleString("de-DE");
+}
+
+function anzahlIndividuen(features) {
+  var summe = 0;
+  features.forEach(item => summe+=parseInt(item.properties.anzahl));
+  
+  return summe.toLocaleString("de-DE")
 }
 
 function top5bundesland(features, bundesland) {
@@ -434,6 +442,7 @@ map.on('load', function() {
   $('#sidebar').css('left', '50px');
   $.ajax({
     url: 'data/beobachtungen-' + param + '.csv',
+    //url: "https://karten.nabu.de/insektensommer/data/beobachtungen-" + param + '.csv',
     method: 'GET',
     success: function(data) {
       makeGeoJSON(data);
@@ -460,10 +469,10 @@ map.on('load', function() {
         }
         top100(data.features);
         $('span.beobachtungen').text(
-          ' Beobachtungen (' + data.features.length + ')'
+          ' Beobachtungen (' + anzahlMeldungen(data.features) + ')' //data.features.length
         );
         $('p.intro-sidebar').append(
-          ' Insgesamt sind <b>' + anzahlMeldungen(data.features) + ' Meldungen </b> eingegangen.'
+          ' Insgesamt sind <b>' + anzahlMeldungen(data.features) + ' Meldungen </b> eingegangen mit <b>' + anzahlIndividuen(data.features) + '</b> Individuen eingegangen.' 
         );
         lebensraumTop5(data.features, 'Garten');
 
